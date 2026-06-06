ad6486ca0e364403afdf30971db225ba77c5af98
Fizza-Khalid41 <fizzakhalid982@gmail.com>
2026-06-06 13:19:09 +0500

    changes in views  and urls and admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, Post, Comment, Network,Job,Conversation,Message
+from .models import Profile, Post, Comment, Network,Job,Conversation,Message,Notification
 
 admin.site.register(Profile)
 admin.site.register(Post)
@@ -8,3 +8,4 @@ admin.site.register(Network)
 admin.site.register(Job)
 admin.site.register(Conversation)
 admin.site.register(Message)
+admin.site.register(Notification)
